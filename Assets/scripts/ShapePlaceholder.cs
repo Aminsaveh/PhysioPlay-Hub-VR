c296244a144dadabeb3af057cf4ed600d8285e25
AminSaveh <Aminsaveh@hotmail.com>
2023-12-10 13:24:27 +0330

    Add Second Game

---
+++ Assets/scripts/ShapePlaceholder.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapePlaceholder : MonoBehaviour
+{
+    public Form form;
+    public Shape shape;
+    public Collider collider;
+    public int index;
+    public MeshRenderer meshRenderer;
+}
