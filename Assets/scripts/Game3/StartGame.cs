703f1ee14f80c7023634f1eed965696f046fbafa
3RFUNn <68325981+3RFUNn@users.noreply.github.com>
2023-12-17 23:06:23 +0330

    Update StartGame.cs

--- Assets/scripts/Game3/StartGame.cs
+++ Assets/scripts/Game3/StartGame.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class StartGame : MonoBehaviour
 {
@@ -8,8 +11,15 @@ public class StartGame : MonoBehaviour
     [SerializeField] private Texture[] baseMaps; 
     [SerializeField] private Renderer quadRenderer;
     [SerializeField] private CartMovement _cartMovement;
+    [SerializeField] private GameObject quad;
     
     private bool assignpic = true;
+
+    private void Start()
+    {
+        quad.SetActive(false);
+        
+    }
 
     // Update is called once per frame
     void Update()
@@ -23,6 +33,11 @@ public class StartGame : MonoBehaviour
             
                 // Apply the texture to the Quad's material
                 quadRenderer.material.mainTexture = selectedTexture;
+                
+                gameObject.SetActive(true);
+                
+                quad.SetActive(true);
+                
             }
 
             assignpic = false;
