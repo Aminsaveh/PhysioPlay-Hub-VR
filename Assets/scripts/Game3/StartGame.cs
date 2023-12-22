c893cea9cfc7e36b0e32311b58c07a9156786744
3RFUNn <68325981+3RFUNn@users.noreply.github.com>
2023-12-22 18:50:40 +0330

    Update StartGame.cs

--- Assets/scripts/Game3/StartGame.cs
+++ Assets/scripts/Game3/StartGame.cs
@@ -10,9 +10,12 @@ public class StartGame : MonoBehaviour
 
     [SerializeField] private Texture[] baseMaps;
     [SerializeField] private GameObject[] objects;
+    [SerializeField] private GameObject[] placeholders;
     [SerializeField] private Renderer quadRenderer;
     [SerializeField] private CartMovement _cartMovement;
     [SerializeField] private GameObject quad;
+
+    private GameObject[] temp = new GameObject[3];
     
     private bool assignpic = true;
     private int selected;
@@ -21,6 +24,7 @@ public class StartGame : MonoBehaviour
     {
         quad.SetActive(false);
         
+
     }
 
     // Update is called once per frame
@@ -30,20 +34,73 @@ public class StartGame : MonoBehaviour
         {
             if (baseMaps.Length > 0 && quadRenderer != null)
             {
-                selected = Random.Range(0, baseMaps.Length);
-                // Randomly pick a texture from the array
-                Texture selectedTexture = baseMaps[selected];
-                print(selectedTexture.name);
-                // Apply the texture to the Quad's material
-                quadRenderer.material.mainTexture = selectedTexture;
                 
-                gameObject.SetActive(true);
-                
-                quad.SetActive(true);
-                
+                PictureAssign();
+
             }
 
             assignpic = false;
         }
+        
     }
+
+    private void PictureAssign()
+    {
+        selected = Random.Range(0, baseMaps.Length);
+        // Randomly pick a texture from the array
+        Texture selectedTexture = baseMaps[selected];
+                
+        // Apply the texture to the Quad's material
+        quadRenderer.material.mainTexture = selectedTexture;
+                
+        gameObject.SetActive(true);
+                
+        quad.SetActive(true);
+        
+        StartCoroutine(PictureTimer());
+        
+    }
+    
+    private int[] TwoRand(int maxRange, int excludeNumber) {
+        int randomNumber1 = 0;
+        int randomNumber2 = 0;
+
+        while (randomNumber1 == excludeNumber) {
+            randomNumber1 = Random.Range(0, maxRange);
+        }
+
+        do {
+            randomNumber2 = Random.Range(0, maxRange);
+        } while (randomNumber2 == excludeNumber || randomNumber2 == randomNumber1);
+
+        return new int[] { randomNumber1, randomNumber2 };
+    }
+
+    private IEnumerator PictureTimer()
+    {
+        yield return new WaitForSeconds(5);
+        quad.SetActive(false);
+        ObjectAssign();
+    }
+
+    private void ObjectAssign()
+    {
+        temp[0] = Instantiate(objects[selected]);
+        int[] two = TwoRand(6, selected);
+        temp[1] = Instantiate(objects[two[0]]);
+        temp[2] = Instantiate(objects[two[1]]);
+        PlaceHolderAssign();
+    }
+
+    private void PlaceHolderAssign()
+    {
+        int rand = Random.Range(0, 100);
+        temp[0].transform.position = placeholders[rand % 3].transform.position;
+        temp[1].transform.position = placeholders[(rand + 1) % 3].transform.position;
+        temp[2].transform.position = placeholders[(rand + 2) % 3].transform.position;
+    }
+    
+    
 }
+
+
